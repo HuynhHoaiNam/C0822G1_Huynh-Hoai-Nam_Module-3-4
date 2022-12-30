632604996a4982027b56f970607d4d08772bfb57
HuynhHoaiNam <hhnam098@gmail.com>
2022-12-30 19:58:25 +0700

    up

--- module_4/case_study/case_spring/src/main/java/com/codegym/repository/facility/IFacilityRepository.java
+++ module_4/case_study/case_spring/src/main/java/com/codegym/repository/facility/IFacilityRepository.java
@@ -9,6 +9,6 @@ import org.springframework.data.repository.query.Param;
 
 public interface IFacilityRepository extends JpaRepository<Facility, Integer> {
 
-    @Query(value = "select * from facility join facility_type ft on ft.id = facility.facility_type_id where ft.name like '%a%'", nativeQuery = true)
-    Page<Facility> listAndSearch(Pageable pageable, @Param("name") String name, @Param("facilityType") String facilityType);
+    @Query(value = "select * from facility join facility_type ft on ft.id = facility.facility_type_id where ft.name like concat( '%',:nameInput,'%')", nativeQuery = true)
+    Page<Facility> listAndSearch(Pageable pageable, @Param("nameInput") String name, @Param("facilityType") String facilityType);
 }
