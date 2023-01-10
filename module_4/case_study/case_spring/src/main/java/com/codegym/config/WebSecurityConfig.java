1a0c9ddc941dc6f43dcf448753a6c83e73860772
HuynhHoaiNam <hhnam098@gmail.com>
2023-01-10 21:23:27 +0700

    up

--- module_4/case_study/case_spring/src/main/java/com/codegym/config/WebSecurityConfig.java
+++ module_4/case_study/case_spring/src/main/java/com/codegym/config/WebSecurityConfig.java
@@ -31,6 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .formLogin()
+                .loginPage("/loginPage")
                 .defaultSuccessUrl("/", true).permitAll()
                 .and().authorizeRequests().antMatchers("/").permitAll()
                 .and()
